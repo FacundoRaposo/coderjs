715b05e67a48b8c26d008a384c141a55f1ee4541
facundo <facuraposo@gmail.com>
2020-12-01 22:43:31 -0300

    desafio 2

---
+++ CLASE 1/script/scripts.js
@@ -0,0 +1,9 @@
+var divisor = 2
+var numero = prompt("Inserta el numero que queres saber si es par");
+Resultado = (numero % divisor);
+if(Resultado == 0){
+    alert("tu numero es par");
+}else{
+    alert("tu numero es impar");
+};
+
