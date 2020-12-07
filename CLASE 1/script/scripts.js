6b64af269eafdf6aef345c1ef8deb7f29c81898f
facundo <facuraposo@gmail.com>
2020-12-07 14:31:02 -0300

    correccion de carpetas

--- CLASE 1/script/scripts.js
+++
@@ -1,9 +0,0 @@
-var divisor = 2
-var numero = prompt("Inserta el numero que queres saber si es par");
-Resultado = (numero % divisor);
-if(Resultado == 0){
-    alert("tu numero es par");
-}else{
-    alert("tu numero es impar");
-};
-
