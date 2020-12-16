4aa5bf793a6c04c5a738c71e65b1371ea33c3985
facundo <facuraposo@gmail.com>
2020-12-16 16:53:09 -0300

    desafio 5

--- desafio 4/script/scripts.js
+++ desafio 4/script/scripts.js
@@ -20,8 +20,39 @@ function costos() {
             costo = monto *(1.24*cuotas);
             alert("Usted pagaria "+cuotas+ " de " +costo+ " Pesos. ");
             break;
+        default:
+            alert("Debe colocar de 2 a 5 cuotas");
+            break;
     }
 
 }
+let plan = {
+	nombre: "cuoti4",
+	limite: 10000,
+	cuotas: 4,
+	tasa: 0.27,
+	vencimiento: 15,
 
+	mostrarInfo: function () {
+		// console.log(this)
+		console.log(`Este producto es: ${this.nombre} y tiene un precio de $${this.limite}`);
+    }   
+}
+
+
+function Plan(nombre, limite, cuotas, tasa, vencimiento) {
+	this.nombre = nombre;
+	this.limite = limite;
+	this.cuotas = cuotas;
+	this.tasa = tasa;
+	this.vencimiento = vencimiento;
+
+	this.mostrarPlanes = function () {
+		console.log(`Este plan es el: ${this.nombre} y tiene una tasa de ${this.tasa} , a pagar en ${this.cuotas} que vencen el dia ${this.vencimiento} de cada mes`);
+    };
+}
+
+const plan2 = new Plan("cuoti3",5000,3,0.15,15);
+
+plan2.mostrarPlanes();
 costos();
